f7a5938734d0d4f12a0db9e69ea1f01416252f9e
Molotov161 <79232216+Molotov161@users.noreply.github.com>
2021-02-18 00:57:00 +0100

    Add files via upload

---
+++ papier.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<H1>molotov</H1>
+<?Php 
+        echo "Bonjours";
+    ?>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet neque accusamus perferendis ratione impedit voluptas, velit quod ab similique nam quia officia, dolorem maxime voluptatibus 
+    sit! Ipsum, sequi? Natus, tenetur.</p>
+    <?Php 
+        echo "Bonjours ";
+        $age = 23;
+        echo 'votre age est ' . $age . ' ans';
+    ?>
+</body>
+</html>
